b29fa553fda348d449d97a7b3b969e3cef869030
Nikita Gupta <sanghinikita@gmail.com>
2023-08-04 20:59:14 +0530

    products api

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
-const port = 8000;
-
+const port = 8080;
+server.use(cors());
 server.use(middleware);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log("server started");
+});
